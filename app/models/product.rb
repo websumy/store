97bcf1e8e9e7c5b5d210c5e5f28346870b936d2e
Vladislav Glazyrin <shepelev.dim@gmail.com>
2016-06-03 15:57:02 +0300

    Added store, added some views

--- app/models/product.rb
+++ app/models/product.rb
@@ -13,7 +13,7 @@
 
 class Product < ActiveRecord::Base
 
-  validates :title, :desciption, :imgae_url, :price, presence: true
+  validates :title, :description, :image_url, :price, presence: true
   validates :price, numericality: { greater_than_or_equal_to: 0 }
   validates :title, uniqueness: true
   validates :image_url, format: {
